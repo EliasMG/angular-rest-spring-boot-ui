2f8231444f380039581d1c2d50be8acb544c2216
EliasMG <elias.b.tec@gmail.com>
2017-11-13 12:52:57 -0300

    Pesquisa de pessoas

---
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent {
+
+  pessoas = [
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' },
+    { nome: 'Manuel Pinheiro', cidade: 'Fortaleza', estado: 'CE',
+      status: 'Inativo' }
+  ];
+}
